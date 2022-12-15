22a915cfac60fe9d7e2c28cc858b7c0237dcbef8
Hans Sjödin <46750749+Dotnetstore@users.noreply.github.com>
2022-12-15 10:58:12 +0100

    Added projects

---
+++ Tests/Dotnetstore.Intranet.Backend.WebAPI.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Dotnetstore.Intranet.Backend.WebAPI.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
